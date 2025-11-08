bfcaa0a650fd904b57cf572a277fd546e67d913d
thpfft <thpfft@yahoo.com>
2025-11-08 13:33:49 -0500

    Update appnew.py

--- backend/appnew.py
+++ backend/appnew.py
@@ -7,7 +7,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
-from utils import gemini
+from gemininew import analyze_image, get_rubric
 import os
 import tempfile
 import json
